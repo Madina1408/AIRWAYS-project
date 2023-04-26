78c68873bf40da3dfb96db7098bee7817be6b017
Melnychuk <IMelnychuk@poland.luxoft.com>
2023-04-26 21:10:33 +0200

    feat: implement routing

--- airways-app/src/app/airways/airways.module.ts
+++ airways-app/src/app/airways/airways.module.ts
@@ -1,12 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectFlightPageComponent } from './pages/booking-pages/select-flight-page/select-flight-page.component';
+import { PassengersPageComponent } from './pages/booking-pages/passengers-page/passengers-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SummaryPageComponent } from './pages/booking-pages/summary-page/summary-page.component';
+import { ShoppingCartPageComponent } from './pages/shopping-cart-page/shopping-cart-page.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: 'main', component: MainPageComponent, },
+  { path: 'booking/step1', component: SelectFlightPageComponent, },
+  { path: 'booking/step2', component: PassengersPageComponent, },
+  { path: 'booking/step3', component: SummaryPageComponent, },
+  { path: 'shopping-cart', component: ShoppingCartPageComponent, },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SelectFlightPageComponent,
+    PassengersPageComponent,
+    MainPageComponent,
+    SummaryPageComponent,
+    ShoppingCartPageComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [
+    SelectFlightPageComponent,
+    PassengersPageComponent,
+    MainPageComponent,
+    SummaryPageComponent,
+    ShoppingCartPageComponent,
+  ],
 })
 export class AirwaysModule { }
--- airways-app/src/app/app-routing.module.ts
+++ airways-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: '',
+  loadChildren: () => import('./airways/airways.module').then((m) => m.AirwaysModule),
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
