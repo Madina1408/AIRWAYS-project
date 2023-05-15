f842cb77ebe256977bceb38a27af74f5d3dd5ffe
Madina1408 <104725156+Madina1408@users.noreply.github.com>
2023-05-15 12:48:49 +0200

    feat:edit button visibility and query params to summary page

--- airways-app/src/app/airways/components/edit-flight-search/edit-flight-search.component.ts
+++ airways-app/src/app/airways/components/edit-flight-search/edit-flight-search.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { IGotFlightData } from 'src/app/shared/models/interfaces/flight-data';
 import { SharedService } from '../../services/shared/shared.service';
+import { RoutesPaths } from 'src/app/shared/models/enums/routes-paths';
 @Component({
   selector: 'app-edit-flight-search',
   templateUrl: './edit-flight-search.component.html',
@@ -18,17 +19,30 @@ export class EditFlightSearchComponent implements OnInit {
   destinationCity: string = '';
   isEditing: boolean = false;
   saveButtonStatus: boolean = false;
+  showEditDiv: boolean = false;
   constructor(
     private activateRoute: ActivatedRoute,
-    private sharedService: SharedService
-  ) {}
+    private sharedService: SharedService,
+    private router: Router
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentUrl = event.url;
+        if (currentUrl.split('/')[2]?.startsWith('step1')) {
+          this.showEditDiv = true;
+        } else {
+          this.showEditDiv = false;
+        }
+      }
+    });
+  }
   ngOnInit(): void {
     this.activateRoute.queryParams.subscribe((params) => {
       this.passengers = params['passengers'];
-      this.forwardDate=params['forwardDate'];
-      this.backDate=params['backDate'];
-      this.destinationCity=params['toCity'];
-      this.departureCity=params['fromCity'];
+      this.forwardDate = params['forwardDate'];
+      this.backDate = params['backDate'];
+      this.destinationCity = params['toCity'];
+      this.departureCity = params['fromCity'];
     });
   }
   editPostRequest() {
--- airways-app/src/app/airways/pages/booking-pages/passengers-page/passengers-page.component.ts
+++ airways-app/src/app/airways/pages/booking-pages/passengers-page/passengers-page.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlightSearchDataService } from 'src/app/airways/services/flight-search-data/flight-search-data.service';
+import { SharedService } from 'src/app/airways/services/shared/shared.service';
+import { RoutesPaths } from 'src/app/shared/models/enums/routes-paths';
+import { ISearchFlight } from 'src/app/shared/models/interfaces/search-flight-interface';
 
 @Component({
   selector: 'app-passengers-page',
   templateUrl: './passengers-page.component.html',
-  styleUrls: ['./passengers-page.component.scss']
+  styleUrls: ['./passengers-page.component.scss'],
 })
-export class PassengersPageComponent {
+export class PassengersPageComponent implements OnInit {
+  queryParams: ISearchFlight = {
+    fromKey: '',
+    toKey: '',
+    forwardDate: '',
+    backDate: '',
+    passengers: '',
+    fromCity: '',
+    toCity: '',
+  };
+  constructor(private activateRoute: ActivatedRoute, private router: Router) {}
 
+  ngOnInit(): void {
+    this.activateRoute.queryParams.subscribe((res) => {
+      this.queryParams.backDate = res['backDate'];
+      this.queryParams.forwardDate = res['forwardDate'];
+      this.queryParams.toCity = res['toCity'];
+      this.queryParams.fromCity = res['fromCity'];
+      this.queryParams.toKey = res['toKey'];
+      this.queryParams.fromKey = res['fromKey'];
+      this.queryParams.passengers = res['passengers'];
+    });
+  }
+
+  continueToNextStep() {
+    this.router.navigate([RoutesPaths.BookingPageStep3], {
+      queryParams: this.queryParams,
+    });
+  }
 }
